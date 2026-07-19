dfbbc00ea350df0403059c1dccdde20fc25b86da
santy-is-dev <santy.nevora@eet3113tobarmosconi.edu.ar>
2026-07-19 15:31:20 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Morfeo iniciado.");
+
+const enterButton = document.getElementById("enterButton");
+
+enterButton.addEventListener("click", () => {
+
+    alert("Bienvenido a Morfeo.");
+
+});
