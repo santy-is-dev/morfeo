146349623478e7ddb159f657538a39edf2b6ab68
santy-is-dev <santy.nevora@eet3113tobarmosconi.edu.ar>
2026-07-19 20:25:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 console.log("Morfeo iniciado.");
 
+const starsContainer = document.getElementById("stars");
+
+for(let i = 0; i < 200; i++){
+
+    const star = document.createElement("div");
+
+    star.classList.add("star");
+
+    const size = Math.random() * 3 + 1;
+
+    star.style.width = size + "px";
+    star.style.height = size + "px";
+
+    star.style.left = Math.random() * 100 + "%";
+    star.style.top = Math.random() * 100 + "%";
+
+    star.style.opacity = Math.random();
+
+    starsContainer.appendChild(star);
+
+}
+
 const enterButton = document.getElementById("enterButton");
 
 enterButton.addEventListener("click", () => {
